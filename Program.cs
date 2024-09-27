eb5226d8d44022dc0036270827a3765d3ab6ad0b
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 16:30:39 -0400

    feat: Service in Scoulfoud Topic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using cis_api_legacy_integration_phase_2.Src.Api.Controllers;
+using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Interfaces;
+using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
+using cis_api_legacy_integration_phase_2.Src.Core.Services;
 using cis_api_legacy_integration_phase_2.Src.Data.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
 
 // Add services to the container.
+builder.Services.AddScoped<IRepositoryGeneric<Topic>, TopicService<Topic>>();
 builder.Services.AddControllers();
 builder.Services.AddScoped<TopicController>();
 builder.Services.AddEndpointsApiExplorer();
