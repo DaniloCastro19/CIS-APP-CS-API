2fc600aa10d4e6888cf2edbd5680f0b32522dcd3
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 16:38:24 -0400

    feat: implements class DTO for Topic

---
+++ Src/Data/DTO/TopicDTO.cs
@@ -0,0 +1,27 @@
+﻿using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace cis_api_legacy_integration_phase_2.Src.Data.DTO
+{
+    public class TopicDTO
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
+        public string Title { get; set; } = null!;
+
+        public string? Creation_date { get; set; }
+
+        public string? UserId { get; set; }
+
+        public static Topic ToCompleteTopic(TopicDTO topicDto)
+        {
+            return new Topic
+            {
+                Id = Guid.NewGuid(),
+                Title = topicDto.Title,
+                Creation_date = topicDto.Creation_date != null ? DateOnly.Parse(topicDto.Creation_date) : null,
+                UserId = topicDto.UserId
+            };
+        }
+    }
+}
