a60f1ae20079a596811db4c6cc4b23f8c5856dbf
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 21:13:18 -0400

    feat: implement ITopicRepositorio with principle dependencies

---
+++ Src/Core/Abstractions/Interfaces/ITopicRepository.cs
@@ -0,0 +1,10 @@
+﻿using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
+
+namespace cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Interfaces
+{
+    public interface ITopicRepository:IRepositoryGeneric<Topic>
+    {
+        Task<IEnumerable<Topic>> GetByTitle(string title);
+        Task<int> CountTopics();
+    }
+}
