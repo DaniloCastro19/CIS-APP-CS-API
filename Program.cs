4f05f4c701909ded80cb0494fbfd65e75ae4f9e7
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 21:08:31 -0400

    fix: modifit in service and repository

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using cis_api_legacy_integration_phase_2.Src.Api.Controllers;
 using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Interfaces;
 using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
+using cis_api_legacy_integration_phase_2.Src.Core.Repository;
 using cis_api_legacy_integration_phase_2.Src.Core.Services;
 using cis_api_legacy_integration_phase_2.Src.Data.Context;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
 
 // Add services to the container.
-builder.Services.AddScoped<IRepositoryGeneric<Topic>, TopicService<Topic>>();
+builder.Services.AddScoped<ITopicRepository, TopicRepository>();
+builder.Services.AddScoped<TopicService>();
 builder.Services.AddControllers();
 builder.Services.AddScoped<TopicController>();
 builder.Services.AddEndpointsApiExplorer();
