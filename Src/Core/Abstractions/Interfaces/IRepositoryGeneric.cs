1dd1aca3dd18134a0cbb4b8dbbc846d321330fb7
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 21:10:17 -0400

    fixt: modifict class in metods Generic

--- Src/Core/Abstractions/Interfaces/IRepositoryGeneric.cs
+++ Src/Core/Abstractions/Interfaces/IRepositoryGeneric.cs
@@ -1,13 +1,15 @@
 using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
 using System;
 
-namespace cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Interfaces;
-
-public interface IRepositoryGeneric<T>: IDisposable where T : class
+namespace cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Interfaces
 {
-    Task<IEnumerable<T>> GetAll();
-    Task<T> GetByID(Guid id);
-    Task<T> Insert(T entity);
-    Task<T> Delete(Guid id);
-    Task Update(T entity);
+    public interface IRepositoryGeneric<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetByID(Guid id);
+        Task<T> Insert(T entity);
+        Task Update(T entity);
+        Task<T> Delete(Guid id);
+        Task Save();
+    }
 }
