35ba70e735a415081765322f5c5bd4951e7076d6
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 21:12:28 -0400

    fixt: implements description in topicDTO

--- Src/Data/DTO/TopicDTO.cs
+++ Src/Data/DTO/TopicDTO.cs
@@ -9,6 +9,7 @@ namespace cis_api_legacy_integration_phase_2.Src.Data.DTO
         [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
         public string Title { get; set; } = null!;
 
+        public string? Description { get; set; }
         public string? Creation_date { get; set; }
 
         public string? UserId { get; set; }
@@ -19,6 +20,7 @@ namespace cis_api_legacy_integration_phase_2.Src.Data.DTO
             {
                 Id = Guid.NewGuid(),
                 Title = topicDto.Title,
+                Description = topicDto.Description,
                 Creation_date = topicDto.Creation_date != null ? DateOnly.Parse(topicDto.Creation_date) : null,
                 UserId = topicDto.UserId
             };
