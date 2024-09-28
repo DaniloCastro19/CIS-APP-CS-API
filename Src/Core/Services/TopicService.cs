66d4eee06c39dfb315b36dd49f0db6677f6bde72
Ochoa42 <ochoaaldoochoa@hotmail.com>
2024-09-27 21:11:49 -0400

    fixt: implements function and conetion with repository

--- Src/Core/Services/TopicService.cs
+++ Src/Core/Services/TopicService.cs
@@ -4,84 +4,40 @@ using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
 using cis_api_legacy_integration_phase_2.Src.Data.Context;
 using Microsoft.EntityFrameworkCore;
 
-namespace cis_api_legacy_integration_phase_2.Src.Core.Services;
-
-public class TopicService<T> : IRepositoryGeneric<T> where T : class
+namespace cis_api_legacy_integration_phase_2.Src.Core.Services
 {
-    private readonly DataContext _dataContext;
-    private bool _disposed = false;
+    public class TopicService
+    {
+        private readonly ITopicRepository _topicRepository;
 
-    public TopicService(DataContext dataContext)
-    {
-        _dataContext = dataContext;
-    }
-    protected DbSet<T> EntitySet
-    {
-        get
+        public TopicService(ITopicRepository topicRepository)
         {
-            return _dataContext.Set<T>();
-        }
-    }
-
-    public async Task<IEnumerable<T>> GetAll()
-    {
-        return await EntitySet.ToListAsync();
-    }
-
-    public async Task<T> GetByID(Guid id)
-    {
-        return await EntitySet.FindAsync(id);
-    }
-
-    public async Task<T> Insert(T entity)
-    {
-        EntitySet.Add(entity);
-        await Save();
-        return entity;
-
-    }
-
-    public async Task Update(T entity)
-    {
-        var id = (entity as dynamic).Id;
-        var existingEntity = await EntitySet.FindAsync(id);
-        if (existingEntity == null)
-        {
-            throw new KeyNotFoundException("Entity not found");
+            _topicRepository = topicRepository;
         }
 
-        _dataContext.Entry(existingEntity).CurrentValues.SetValues(entity);
-        await Save();
-    }
-
-    public async Task<T> Delete(Guid id)
-    {
-        T entity = await EntitySet.FindAsync(id);
-        EntitySet.Remove(entity);
-        await Save();
-        return entity;
-    }
-
-    public async Task Save()
-    {
-        await _dataContext.SaveChangesAsync();
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!_disposed)
+        public async Task<IEnumerable<Topic>> GetAllTopics()
         {
-            if (disposing)
-            {
-                _dataContext.Dispose();
-            }
+            return await _topicRepository.GetAll();
         }
-        this._disposed = true;
-    }
 
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
+        public async Task<Topic> GetTopicById(Guid id)
+        {
+            return await _topicRepository.GetByID(id);
+        }
+
+        public async Task<Topic> CreateTopic(Topic topic)
+        {
+            return await _topicRepository.Insert(topic);
+        }
+
+        public async Task UpdateTopic(Topic topic)
+        {
+            await _topicRepository.Update(topic);
+        }
+
+        public async Task<Topic> DeleteTopic(Guid id)
+        {
+            return await _topicRepository.Delete(id);
+        }
     }
 }
