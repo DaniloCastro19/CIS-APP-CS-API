89e8261cd737f0420b8f06d151a71bbee6965898
Dan_19 <Danilo.Castro@jala.university>
2024-10-10 21:37:20 -0500

    fix: moving validations to they folder

--- Src/Core/Services/VoteService.cs
+++ Src/Core/Services/VoteService.cs
@@ -1,6 +1,7 @@
 using cis_api_legacy_integration_phase_2.Core.Abstractions.Models;
 using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Interfaces;
 using cis_api_legacy_integration_phase_2.Src.Core.Abstractions.Models;
+using cis_api_legacy_integration_phase_2.Src.Core.Validations;
 using cis_api_legacy_integration_phase_2.Src.Core.Utils;
 using cis_api_legacy_integration_phase_2.Src.Data.DTO;
 using Microsoft.AspNetCore.Http.HttpResults;
